9f25467e16478b5aaf40bed92d8dbff8e53c1096
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-30 11:01:30 -0700

    Add team_info methods to module

--- modules/teamable.rb
+++ modules/teamable.rb
@@ -1,3 +1,17 @@
 module Teamable
+  def team_info(team_id)
+    info = {
+      team_id: get_team_with_id(team_id).team_id,
+      franchise_id: get_team_with_id(team_id).franchise_id,
+      team_name: get_team_with_id(team_id).team_name,
+      abbreviation: get_team_with_id(team_id).abbreviation
+    }
+    info
+  end
 
+  def get_team_with_id(team_id)
+    @teams.find do |team|
+      team.team_id == team_id
+    end
+  end
 end
