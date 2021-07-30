6c91acde89e5000ea4138b941a33bf1cb75e81b4
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 21:00:09 -0700

    Add highest total score to module

--- modules/gameable.rb
+++ modules/gameable.rb
@@ -1,3 +1,8 @@
 module Gameable
-  
+  def highest_total_score
+    array_highest_sum_teams_scores = @games.map do |game|
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+    array_highest_sum_teams_scores.max
+  end
 end
