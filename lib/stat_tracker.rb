aecc0e295993846c4594c7e7e1373795e00524d3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 20:06:43 -0700

    Include modules

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,20 @@
+require_relative '../lib/games'
+require_relative '../lib/teams'
+require_relative '../lib/game_teams'
+
 class StatTracker
-  
-end
+  include Gameable
+  include Teamable
+  include GameTeamable
+
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(files)
+    @games ||= Game.file(files[:games])
+    @teams ||= Team.file(files[:teams])
+    @game_teams ||= GameTeam.file(files[:game_teams])
+  end
+
+  def self.from_csv(files)
+    new(files)
+  end
