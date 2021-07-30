5135a60f2204e7a8db684a2f32e99f3529640b5b
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 20:35:28 -0700

    Change instance variable

--- test/stat_tracker_spec.rb
+++ test/stat_tracker_spec.rb
@@ -26,8 +26,7 @@ describe StatTracker do
     end
 
     it 'has games' do
-      expect(@stat_tracker.games.all? { |game| game.class == Game }).to eq(true)
-      expect(@stat_tracker.teams.all? { |team| team.class == Game }).to eq(true)
+      expect(@stat_tracker.teams.all? { |team| team.class == Team }).to eq(true)
     end
   end
 end
