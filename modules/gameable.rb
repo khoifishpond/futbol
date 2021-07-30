86cb1fc035b2b323df71527fccbc24991c2ef696
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 21:14:29 -0700

    Create helper method

--- modules/gameable.rb
+++ modules/gameable.rb
@@ -1,8 +1,15 @@
 module Gameable
   def highest_total_score
-    array_highest_sum_teams_scores = @games.map do |game|
+    all_total_scores.max
+  end
+
+  def lowest_total_score
+    all_total_scores.min
+  end
+
+  def all_total_scores
+    @games.map do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
-    array_highest_sum_teams_scores.max
   end
 end
