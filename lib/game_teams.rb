c6bb64f6362ca172f4eb170e8d41f33e62b4fa2f
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 21:32:54 -0700

    Add inctance variable hoa

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -6,6 +6,7 @@ class GameTeam
 
   attr_reader :game_id,
               :team_id,
+              :hoa,
               :result,
               :head_coach,
               :goals,
@@ -15,6 +16,7 @@ class GameTeam
   def initialize(row)
     @game_id ||= row["game_id"]
     @team_id ||= row["team_id"]
+    @hoa ||= row["HoA"]
     @result ||= row["result"]
     @head_coach ||= row["head_coach"]
     @goals ||= row["goals"]
