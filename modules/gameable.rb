99e7345b737f0513d1bbbeaab9777241522fe0bf
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 21:54:24 -0700

    Add percentage home wins to module

--- modules/gameable.rb
+++ modules/gameable.rb
@@ -12,4 +12,20 @@ module Gameable
       game.away_goals.to_i + game.home_goals.to_i
     end
   end
+
+  def percentage_home_wins
+    win_count = hash_hoa_game_teams["home"].count do |game_team|
+      game_team.result == "WIN"
+    end
+    all_games = hash_hoa_game_teams["home"].count do |game_team|
+      game_team
+    end
+    win_count.fdiv(all_games).round(2)
+  end
+
+  def hash_hoa_game_teams
+    @game_teams.group_by do |game_team|
+      game_team.hoa
+    end
+  end
 end
