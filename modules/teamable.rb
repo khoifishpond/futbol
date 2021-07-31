e8c82ae743a0e44f56943c08db52f27681e7af34
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-30 19:06:04 -0700

    Add best/worst season for a team methods to module

--- modules/teamable.rb
+++ modules/teamable.rb
@@ -14,4 +14,38 @@ module Teamable
       team.team_id == team_id
     end
   end
+
+  def best_season(team_id)
+    seasons_win_percentage = hash_season_games_won_percentage_by_team_id(team_id)
+    seasons_win_percentage.key(seasons_win_percentage.values.max)
+  end
+
+  def worst_season(team_id)
+    seasons_win_percentage = hash_season_games_won_percentage_by_team_id(team_id)
+    seasons_win_percentage.key(seasons_win_percentage.values.min)
+    require 'pry'; binding.pry
+  end
+  
+  def hash_season_games_won_percentage_by_team_id(team_id)
+    season_games_won_percentage = {}
+    hash_games_by_season(team_id).each do |key_season, value_games|
+      season_games_won_percentage[key_season] = value_games.count do |game|
+        (game.away_team_id == team_id && game.away_goals > game.home_goals) ||
+        (game.home_team_id == team_id && game.home_goals > game.away_goals)
+      end.fdiv(value_games.count).round(2)
+    end
+    season_games_won_percentage
+  end
+
+  def hash_games_by_season(team_id)
+    array_games_by_team_id(team_id).group_by do |game|
+      game.season
+    end
+  end
+
+  def array_games_by_team_id(team_id)
+    @games.find_all do |game|
+      game.away_team_id == team_id || game.home_team_id == team_id
+    end
+  end
 end
